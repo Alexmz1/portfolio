92046896eaee4c1080c272058b772ac38d6dae07
Alexmz1 <menez.alexis19@gmail.com>
2025-02-14 14:01:04 +0100

    Page layout for sections and background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import Header from "../components/header";
+import About from "@/components/about";
+import Project from "@/components/project";
+import Contact from "@/components/contact";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-gradient-to-b from-black to-purple-950"> 
       <Header />
 
+      <About />
+
+      <Project />
+
+      <Contact />
+
+      <Footer />
     </div>
   );
 }
