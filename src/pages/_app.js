9dd8584df812b46bd5ce2601cc7fb6368725f63c
Alexmz1 <menez.alexis19@gmail.com>
2025-04-28 11:49:57 +0200

    Modification portfolio cover img

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
                 <title>Mon portfolio - Alexis</title>
                 <meta property="og:title" content="Mon Portfolio - Alexis" />
                 <meta property="og:description" content="Découvrez mes projets en développement web et design graphique." />
-                <meta property="og:image" content="/images/profilePicture/profilePicutre2.jpeg" />
+                <meta property="og:image" content="/images/developmentProject/portfolio.png" />
                 <meta property="og:url" content="https://portfolio-alexis-menez.vercel.app" />
             </Head>
             <Component {...pageProps} />
